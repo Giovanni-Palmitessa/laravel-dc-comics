d22b8a3a4c3ee2c2e25971de399626d256b6f45b
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-29 14:46:07 +0200

    add description in row

--- database/migrations/2023_06_29_124113_create_comics_table.php
+++ database/migrations/2023_06_29_124113_create_comics_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
             $table->string('title', 60);
+            $table->text('description');
             $table->timestamps();
         });
     }
