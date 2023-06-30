4c99ed5c7a46e0f5d0bd1849a2fcbbc59205bbc6
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-30 18:00:40 +0200

    add button to come back to comics page

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.base')
 
 @section('contents')
-<div class="card">
+<div class="card-container d-flex justify-content-center py-5">
+  <div class="card p-2">
     <img src="{{$comic->thumb}}" class="card-img-top" alt="{{$comic->title}}">
     <div class="card-body">
       <h5 class="card-title">{{$comic->title}}</h5>
@@ -13,5 +14,11 @@
       <li class="list-group-item">{{$comic->series}}</li>
       <li class="list-group-item">In Vendita dal: {{$comic->sale_date}}</li>
     </ul>
+  </div>
 </div>
+
+<div class="d-flex justify-content-center">
+  <button type="button" class="btn btn-primary my-2"><a href="/comics" class="text-center text-white">Torna indietro ai fumetti</a></button>
+</div>
+
 @endsection
